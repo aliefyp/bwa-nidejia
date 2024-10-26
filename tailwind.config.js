193dbfb8617d1ee0067f4416cef2a3f31619a82a
Alief Yoga Priyanto <aliefyp@gmail.com>
2024-10-26 19:48:38 +0700

    feat: prettify tailwind classes with prettier

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.export = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        brand: "#FF9357",
+        secondary: "#323437",
+        dark: "#141414",
+        dark2: "#202020",
+        steel: "#A8A8AB",
+      },
+    },
+  },
+};
